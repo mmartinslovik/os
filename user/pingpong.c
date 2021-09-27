a9c6caaaee4a3e633764bf83d768703335fea458
os2021 <os2021@os2021>
2021-09-27 09:19:40 +0200

    feat: implement pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,39 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int to_parent[2];
+  int to_child[2];
+
+  pipe(to_parent);
+  pipe(to_child);
+
+  if(pipe(to_parent) == -1 || pipe(to_child) == -1){
+    fprintf(2, "pipe error occured");
+    exit(-1);
+  }
+
+  int pid = fork();
+
+  if(pid < 0){
+    fprintf(2, "fork unsuccessful");
+    exit(-1);
+  }
+  else if(pid == 0){
+    char received;
+    read(to_child[0], &received, 1);
+    printf("%d: received ping\n", getpid());
+    write(to_parent[1], "a", 1);
+  } else {
+    write(to_child[1], "a", 1);
+    char received;
+    read(to_parent[0], &received, 1);
+    printf("%d: received pong\n", getpid());
+  }
+
+  exit(0);
+}
+
