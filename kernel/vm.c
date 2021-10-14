ba06a6eed12fe7cf55427b243e159b37931608fa
os2021 <os2021@os2021>
2021-10-14 14:07:11 +0200

    feat: implement vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,33 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+// Print lower-level page table using recursion.
+void
+nest_vmprint(pagetable_t pagetable, int depth)
+{
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      for(int j = 0; j <= depth; j++)
+	printf(".. ");
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+    }
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      nest_vmprint((pagetable_t)child, depth+1);
+    }
+  }    
+}
+
+// Print contents of page table. 
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  nest_vmprint(pagetable, 0);
+}
+
+
