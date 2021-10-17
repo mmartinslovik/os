916856734b5bc322f866ae2d1c40dd85103555e5
os2021 <os2021@os2021>
2021-10-17 10:54:47 +0200

    feat: implement pgaccess

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -77,10 +77,39 @@ sys_sleep(void)
 
 
 #ifdef LAB_PGTBL
+// indicates the pages which were accessed in the past
 int
 sys_pgaccess(void)
 {
-  // lab pgtbl: your code here.
+  uint64 start;
+  int npages;
+  uint64 bitmask_ua;
+  uint64 bitmask_k = 0;
+ 
+  if(argaddr(0, &start) < 0)
+    return -1;
+  if(argint(1, &npages) < 0)
+    return -1;
+  if(argaddr(2, &bitmask_ua) < 0)
+    return -1;
+  
+  // check if arguments are valid
+  if(npages > sizeof(int)*8)
+    npages = sizeof(int)*8;
+
+  int size = (npages+7)/8;
+  for(int i = 0; i < npages; i++){
+    // search the right pte 
+    pte_t *pte = walk(myproc()->pagetable, start, 0);
+    if(pte != 0 && *pte & PTE_A){
+      bitmask_k |= 1 << i;
+      *pte &= ~PTE_A;
+    }
+    start += PGSIZE;
+  }
+  // copy kernel bitmask to the user space 
+  if(copyout(myproc()->pagetable, bitmask_ua, (char *) &bitmask_k, size))
+    return -1;
   return 0;
 }
 #endif
