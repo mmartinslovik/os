31a8d0d7202ea6b21106829abfb23ed87b1fd424
os2021 <os2021@os2021>
2021-10-22 19:49:47 +0200

    feat: implement backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -122,6 +122,7 @@ panic(char *s)
   printf(s);
   printf("\n");
   panicked = 1; // freeze uart output from other CPUs
+  backtrace();
   for(;;)
     ;
 }
@@ -131,4 +132,18 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void
+backtrace(void)
+{
+  printf("backtrace: \n");
+  uint64 fp = r_fp();
+  uint64 ret_addr;
+
+  while(PGROUNDDOWN(fp) < PGROUNDUP(fp)){
+    ret_addr = *(uint64*)(fp-8);
+    printf("%p\n", ret_addr);
+    fp = *(uint64*)(fp-16);
+  }
 }
